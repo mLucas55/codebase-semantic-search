4d2ef1feece17543328f4d9c5b391a1ff91b1ab8
mlucas55 <lucasmonroe66@gmail.com>
2025-12-17 12:15:21 -0500

    Working api call

--- index.py
+++ index.py
@@ -1,15 +1,17 @@
 from google import genai
+
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-api_key = os.getenv("")
+GEMENI_KEY = os.getenv("GEMENI_KEY")
 
-client = genai.Client()
+client = genai.Client(api_key=GEMENI_KEY)
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Explain how AI works in a few words",
+    model="gemini-3-flash-preview",
+    contents="Tell me about typesrcipt in a few words",
 )
 
 print(response.text)
